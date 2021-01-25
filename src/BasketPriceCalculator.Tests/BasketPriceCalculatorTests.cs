0c7b2a5a4c6d4f9db38bac0d942370af9e5263c8
driznar <driznar@gmail.com>
2021-01-25 05:07:43 +0000

    Adding 2nd (1st scenario) failing test

--- src/BasketPriceCalculator.Tests/BasketPriceCalculatorTests.cs
+++ src/BasketPriceCalculator.Tests/BasketPriceCalculatorTests.cs
@@ -31,5 +31,26 @@ namespace BasketPriceCalculator.Tests
 
             Assert.That(result, Is.Zero);
         }
+
+        [Test]
+        public void Calculate_OneButterOneMilkOneBread_ReturnsNonZeroTotal()
+        {
+            var repositoryMock = new Mock<IBasketRepository>();
+            repositoryMock
+                .Setup(i => i.GetAll())
+                .Returns(
+                    new List<BasketItem>
+                    {
+                        new BasketItem { Product=new Product{ Name="Bread", Price=1.0m }, Quantity=1 },
+                        new BasketItem { Product=new Product{ Name="Milk", Price=1.15m }, Quantity=1 },
+                        new BasketItem { Product=new Product{ Name="Butter", Price=0.8m }, Quantity=1 }
+                    }
+                );
+
+            _sut = new BasketPriceCalculatorService(repositoryMock.Object);
+            var result = _sut.CalculateTotal();
+
+            Assert.That(result, Is.EqualTo(2.95m));
+        }
     }
 }
