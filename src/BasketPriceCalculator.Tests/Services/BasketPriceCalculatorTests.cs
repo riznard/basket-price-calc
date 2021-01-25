217130d9fe9300264c18042e121a91c3af6561cc
driznar <driznar@gmail.com>
2021-01-25 08:49:31 +0000

    Cleaning up the calculator tests a bit

---
+++ src/BasketPriceCalculator.Tests/Services/BasketPriceCalculatorTests.cs
@@ -0,0 +1,134 @@
+using NUnit.Framework;
+using Moq;
+using BasketPriceCalculatorService = BasketPriceCalculator.Services.BasketPriceCalculator;
+using BasketPriceCalculator.Repositories;
+using BasketPriceCalculator.Entities;
+using System.Collections.Generic;
+
+namespace BasketPriceCalculator.Services.Tests
+{
+    public class BasketPriceCalculatorTests
+    {
+        private BasketPriceCalculatorService _sut;
+
+        [Test]
+        public void Calculate_EmptyBasket_ReturnsZeroTotal()
+        {
+            var repositoryMock = new Mock<IBasketRepository>();
+            repositoryMock
+                .Setup(i => i.GetAll())
+                .Returns(
+                    new List<BasketItem>()
+                );
+
+            _sut = new BasketPriceCalculatorService(repositoryMock.Object);
+            var result  = _sut.CalculateTotal();
+
+            Assert.That(result, Is.Zero);
+        }
+
+        [Test]
+        public void Calculate_OneButterOneMilkOneBread_ReturnsNonZeroTotal()
+        {
+            var repositoryMock = new Mock<IBasketRepository>();
+            repositoryMock
+                .Setup(i => i.GetAll())
+                .Returns(
+                    new List<BasketItem>
+                    {
+                        new BasketItem { Product=new Product{ Name="Bread", Price=1.0m }, Quantity=1 },
+                        new BasketItem { Product=new Product{ Name="Milk", Price=1.15m }, Quantity=1 },
+                        new BasketItem { Product=new Product{ Name="Butter", Price=0.8m }, Quantity=1 }
+                    }
+                );
+
+            _sut = new BasketPriceCalculatorService(repositoryMock.Object);
+            var result = _sut.CalculateTotal();
+
+            Assert.That(result, Is.EqualTo(2.95m));
+        }
+
+        [Test]
+        public void Calculate_OneButterTwoBreadTwoMilk_ReturnsNonZeroTotal()
+        {
+            var repositoryMock = new Mock<IBasketRepository>();
+            repositoryMock
+                .Setup(i => i.GetAll())
+                .Returns(
+                    new List<BasketItem>
+                    {
+                        new BasketItem { Product=new Product{ Name="Bread", Price=1.0m }, Quantity=2 },
+                        new BasketItem { Product=new Product{ Name="Milk", Price=1.15m }, Quantity=2 },
+                        new BasketItem { Product=new Product{ Name="Butter", Price=0.8m }, Quantity=1 }
+                    }
+                );
+
+            _sut = new BasketPriceCalculatorService(repositoryMock.Object);
+            var result = _sut.CalculateTotal();
+
+            Assert.That(result, Is.EqualTo(5.1m));
+        }
+
+        [Test]
+        public void Calculate_TwoButterTwoBread_ReturnsNonZeroTotalWithoutDiscount()
+        {
+            var repositoryMock = new Mock<IBasketRepository>();
+            repositoryMock
+                .Setup(i => i.GetAll())
+                .Returns(
+                    new List<BasketItem>
+                    {
+                        new BasketItem { Product=new Product{ Name="Bread", Price=1.0m }, Quantity=2 },
+                        new BasketItem { Product=new Product{ Name="Butter", Price=0.8m }, Quantity=2 }
+                    }
+                );
+
+            _sut = new BasketPriceCalculatorService(repositoryMock.Object);
+            var result = _sut.CalculateTotal();
+
+            Assert.That(result, Is.EqualTo(3.6m));
+        }
+
+        [Test]
+        public void Calculate_FourMilk_ReturnsNonZeroTotalWithoutDiscount()
+        {
+            var repositoryMock = new Mock<IBasketRepository>();
+            repositoryMock
+                .Setup(i => i.GetAll())
+                .Returns(
+                    new List<BasketItem>
+                    {
+                        new BasketItem { Product=new Product{ Name="Milk", Price=1.15m }, Quantity=4 }
+                    }
+                );
+
+            _sut = new BasketPriceCalculatorService(repositoryMock.Object);
+            var result = _sut.CalculateTotal();
+
+            Assert.That(result, Is.EqualTo(4.6m));
+        }
+
+        [Test]
+        public void Calculate_TwoButterOneBreadEightMilk_ReturnsNonZeroTotalWithoutDiscount()
+        {
+            var repositoryMock = new Mock<IBasketRepository>();
+            repositoryMock
+                .Setup(i => i.GetAll())
+                .Returns(
+                    new List<BasketItem>
+                    {
+                        new BasketItem { Product=new Product{ Name="Bread", Price=1.0m }, Quantity=1 },
+                        new BasketItem { Product=new Product{ Name="Milk", Price=1.15m }, Quantity=8 },
+                        new BasketItem { Product=new Product{ Name="Butter", Price=0.8m }, Quantity=2 }
+
+                    }
+                );
+
+            _sut = new BasketPriceCalculatorService(repositoryMock.Object);
+            var result = _sut.CalculateTotal();
+
+            Assert.That(result, Is.EqualTo(11.8m));
+        }
+
+    }
+}
