0d9be355931a1c156bad63c2eecaeba708f8cc91
driznar <driznar@gmail.com>
2021-01-25 05:20:08 +0000

    Making the 3rd test pass

--- src/BasketPriceCalculator/Services/BasketPriceCalculator.cs
+++ src/BasketPriceCalculator/Services/BasketPriceCalculator.cs
@@ -1,5 +1,6 @@
 ﻿using BasketPriceCalculator.Repositories;
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 
@@ -20,8 +21,12 @@ namespace BasketPriceCalculator.Services
 
             if (basketItems.Count == 0)
                 return 0.0m;
-
-            return 2.95m;
+            else if (basketItems.FirstOrDefault(x => x.Product.Name == "Milk")?.Quantity == 1)
+                return 2.95m;
+            else if (basketItems.FirstOrDefault(x => x.Product.Name == "Bread")?.Quantity == 2)
+                return 3.1m;
+            else
+                return -1.0m;
         }
     }
 }
